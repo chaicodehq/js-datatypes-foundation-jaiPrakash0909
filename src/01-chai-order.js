3d17cd88740afd8b611c54b9e94a9058345e32f2
Jai prakash Banjare <148434289+Jai932@users.noreply.github.com>
2026-04-04 02:39:58 +0530

    completed chai-order program

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -46,21 +46,49 @@
  *   hasSpecialIngredient("Elaichi Chai", "elaichi")  // => true
  */
 export function getChaiOrderLength(order) {
-  // Your code here
+  
+  if(typeof(order) !== 'string'){
+    return -1;
+  }
+  return order.trim().length;
+  
 }
 
 export function shoutChaiOrder(order) {
-  // Your code here
+  
+  if(typeof(order) !== 'string'){
+    return "";
+  }
+  return order.trim().toUpperCase();
+
 }
 
 export function whisperChaiOrder(order) {
-  // Your code here
+  
+  if(typeof(order) !== 'string'){
+    return "";
+  }
+  return order.trim().toLowerCase();
 }
 
 export function hasSpecialIngredient(order, ingredient) {
-  // Your code here
+
+  if(typeof(order) !== 'string' || typeof(ingredient) !== 'string'){
+    return false;
+  }
+  
+  return order.toLowerCase().includes(ingredient.toLowerCase());
+  
 }
 
 export function getFirstAndLastChar(order) {
-  // Your code here
+
+  if(typeof(order) !== 'string' || order.trim() ===""){
+    return null;
+  }
+
+  let newOrder = order.trim();
+  let first = newOrder.charAt(0);
+  let last = newOrder.at(-1);
+  return {first, last};
 }
